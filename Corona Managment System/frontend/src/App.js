f9e28ce070001665a7776d894b78ce948eeb7002
Rachel2070 <rachelwe939@gmail.com>
2024-03-25 23:44:32 +0200

    presenting member list

--- Corona Managment System/frontend/src/App.js
+++ Corona Managment System/frontend/src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './Layout/Layout';
+import Members from './Members/Members';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Members />} />
+        </Routes>
+      </Layout>
+    </Router>
+
+  )
 }
 
 export default App;
---
+++ Corona Managment System/frontend/src/Layout/Layout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from '@mui/material';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+import IconButton from '@mui/material/IconButton';
+import './Layout.css'
+
+
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <header className="header">
+                <h1>Covid Management System</h1>
+                <nav>
+                    <ul className="nav-links">
+                        <li><Link to="/">Home</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="main-content">
+                {children}
+            </main>
+            <footer className="footer">
+                © 2023 My Application. All rights reserved.
+                <IconButton>
+                    <AccessibleIcon color="primary"/>
+                </IconButton>
+            </footer>
+        </div>)
+};
+
+export default Layout;
