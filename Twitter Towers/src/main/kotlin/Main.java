4bd3a90c68ecf09d77cefa4e582fd669037a9bc3
Rachel2070 <rachelwe939@gmail.com>
2024-03-27 17:01:38 +0200

    add files

---
+++ Twitter Towers/src/main/kotlin/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello, and welcome to Twitter Towers");
+        System.out.println("These are our towers options:");
+        while (true) {
+            presentTowerOptions();
+            int choice = in.nextInt();
+
+            if (choice == 1) {
+                handleRectangularTower(in);
+            }else if (choice == 2) {
+                handleTriangularTower(in);
+            } else if (choice == 3) {
+                System.out.println("Exiting program.");
+                break;
+            } else {
+                System.out.println("Invalid input, please try again");
+                System.out.println();
+            }
+        }
+    }
+
+    private static void presentTowerOptions(){
+        System.out.println("1. Rectangular Tower");
+        System.out.println("2. Triangular Tower");
+        System.out.println("3. Exit");
+        System.out.println("Enter your choice:");
+    }
+
+    private static void handleRectangularTower(Scanner in) {
+        System.out.println("Enter height");
+        int height = in.nextInt();
+        System.out.println("Enter width");
+        int width = in.nextInt();
+
+        TowerFunctions.rectangularTower(height, width);
+    }
+
+    private static void handleTriangularTower(Scanner in) {
+        System.out.println("Enter height");
+        int height = in.nextInt();
+        System.out.println("Enter width");
+        int width = in.nextInt();
+
+        TowerFunctions.triangularTower(height, width, in);
+    }
+
+    }
---
+++ Twitter Towers/src/main/kotlin/TowerFunctions.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TowerFunctions {
+    Scanner in = new Scanner(System.in);
+    public static void rectangularTower(int height, int width){
+        if(height == width || Math.abs(height-width)>5){
+            System.out.println("The tower's area: " + height * width);
+            System.out.println();
+        }
+        else {
+            System.out.println("The tower's perimeter: " + 2 * (height + width));
+            System.out.println();
+        }
+    }
+
+    public static void triangularTower(int height, int width, Scanner in){
+        PresentTriangularOptions();
+        int choice = in.nextInt();
+
+        if(choice==1){
+            int perimeter = 2 * height + width;
+            System.out.println("The tower's perimeter: " + perimeter);
+
+            System.out.println();
+        }
+        else if (choice==2) {
+            if(width % 2 == 0 || width > height * 2){
+                System.out.println("The triangle cannot be printed");
+                System.out.println();
+            }else {
+                printTriangular(height, width);
+            }
+        }
+    }
+
+    private static void printTriangular(int height, int width){
+        List<Integer> oddNumbers = oddNumbersCnt(width);
+
+        if(oddNumbers.size()==0){
+            oddNumbers.add(1);
+        }
+
+        int linesPerNumber = (height-2)/oddNumbers.size();
+        int extraLines = (height-2)%oddNumbers.size();
+        StringBuilder spaces=new StringBuilder();
+
+        for (int j = 0; j < Math.floor(width/2); j++) {
+            spaces.append(" ");
+        }
+
+        StringBuilder stars = new StringBuilder("*");
+        String starsToAdd = "**";
+
+        System.out.print(spaces);
+        System.out.println(stars);
+        if (oddNumbers.get(0)!=1) {
+            stars.append(starsToAdd);
+        }
+        if(spaces.length()>1){
+            spaces.deleteCharAt(0);
+        }
+
+        for (int j = 0; j < extraLines; j++) {
+            System.out.print(spaces);
+            System.out.println(stars);
+        }
+
+        for (int i = 0; i < oddNumbers.size(); i++) {
+
+            for (int j = 0; j < linesPerNumber; j++) {
+                System.out.print(spaces);
+                System.out.println(stars);
+            }
+
+            if (spaces.length() > 0) {
+                spaces.deleteCharAt(0);
+            }
+                stars.append(starsToAdd);
+
+        }
+        System.out.println(stars);
+    }
+
+    private  static void PresentTriangularOptions(){
+        System.out.println("Choose an option:");
+        System.out.println("1. Calculate the perimeter of the triangle");
+        System.out.println("2. Print the triangle");
+        System.out.println("Enter your choice:");
+    }
+
+    private static List<Integer> oddNumbersCnt(int num){
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (int i = 3; i < num; i+=2) {
+                oddNumbers.add(i);
+        }
+        return oddNumbers;
+    }
+
+}
+
+
+
